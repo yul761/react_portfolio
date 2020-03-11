0f3853fcf6d22e000c2e7296ef9eae7b55e44aa8
Yuchen Lin <kloselyc@gmail.com>
2020-03-10 17:34:26 -0700

    Updates

---
+++ precache-manifest.12c4d34ee27d8819fd6bd9e0832d0f73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e4a4ba9b97c125fb67dc7aa2a837e8",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "59345940104ec5d1d223",
+    "url": "/react_portfolio/static/css/main.78c2bd90.chunk.css"
+  },
+  {
+    "revision": "c2dcaa29b8971b89cbba",
+    "url": "/react_portfolio/static/js/2.200bc569.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_portfolio/static/js/2.200bc569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59345940104ec5d1d223",
+    "url": "/react_portfolio/static/js/main.e4017c51.chunk.js"
+  },
+  {
+    "revision": "373616d158445d08ad1f",
+    "url": "/react_portfolio/static/js/runtime-main.e648aeeb.js"
+  },
+  {
+    "revision": "4d685f872d5088e849e6feddc9caf6e9",
+    "url": "/react_portfolio/static/media/Contact.4d685f87.jpg"
+  },
+  {
+    "revision": "eb8d905187967a0dd98a363dd6090b6a",
+    "url": "/react_portfolio/static/media/ContactWithInfo.eb8d9051.png"
+  },
+  {
+    "revision": "db45a1d51c8775f7a18aa13d7e5c483b",
+    "url": "/react_portfolio/static/media/IMG_5090.db45a1d5.JPG"
+  },
+  {
+    "revision": "0069a4e9c3dd375c06b7a41a5d65431a",
+    "url": "/react_portfolio/static/media/ResumeIcon.0069a4e9.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/react_portfolio/static/media/githubIcon.3e54ed15.png"
+  },
+  {
+    "revision": "acaf6e71303494905bd323c51552b7b3",
+    "url": "/react_portfolio/static/media/steadyWelife.acaf6e71.PNG"
+  },
+  {
+    "revision": "e74ae6066c6f827571442b6152b7c172",
+    "url": "/react_portfolio/static/media/welifeVideo.e74ae606.mp4"
+  }
+]);
