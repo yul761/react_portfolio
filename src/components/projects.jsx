cdce0b9b78ffd59a745aeb0cc3de82a22bc5631b
Yuchen Lin <kloselyc@gmail.com>
2020-03-26 13:57:42 -0600

    update portfolio

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import SteadyWelife from "../assets/steadyWelife.PNG";
 import welifeVideo from "../assets/welifeVideo.mp4";
+import CovidPoster from "../assets/COVID-19-poster.png";
+import CovidVideo from "../assets/COVID_19_Tracker_videosample.mov";
 
 export default class projects extends Component {
   render() {
@@ -52,6 +54,72 @@ export default class projects extends Component {
                 </li>
                 <li className="projects__content-welife--description--details--content">
                   Use ‘axios’ to communicate between frontend and backend.
+                </li>
+                <li className="projects__content-welife--description--details--content">
+                  Deployed address :{" "}
+                  <a href="https://welife.herokuapp.com">
+                    https://welife.herokuapp.com
+                  </a>
+                </li>
+                <li className="projects__content-welife--description--details--content">
+                  Github address :{" "}
+                  <a href="https://github.com/yul761/WeLife">
+                    https://github.com/yul761/WeLife
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/******************* COVID 19 Tracker***************************/}
+        <div className="projects__content">
+          <div className="projects__content-covidTracker">
+            <div className="projects__content-covidTracker--title">
+              COVID 19 Tracker
+            </div>
+            <div className="projects__content-covidTracker--preview">
+              <div className="projects__content-covidTracker--preview--container">
+                <video
+                  className="projects__content-covidTracker--preview--container--src"
+                  alt="steady_welife"
+                  poster={CovidPoster}
+                  controls
+                  autoPlay
+                >
+                  <source src={CovidVideo} type="video/mp4"></source>
+                </video>
+              </div>
+            </div>
+            <div className="projects__content-covidTracker--description">
+              <ul className="projects__content-covidTracker--description--details">
+                <li className="projects__content-covidTracker--description--details--content">
+                  A web application that visualize and track the data of
+                  COVID_19 ,such as total cases, total deaths ..., to have a
+                  better understanding how COVID_19 is impacting the whole
+                  world.
+                </li>
+                <li className="projects__content-covidTracker--description--details--content">
+                  Use 'react-chartjs-2' package to create the chart
+                </li>
+                <li className="projects__content-covidTracker--description--details--content">
+                  Use 'react-select' package to create a searchable dropdown bar
+                </li>
+                <li className="projects__content-covidTracker--description--details--content">
+                  Data is fetched using 'axios' from the API called 'Coronavirus
+                  monitor' created by 'astsiatsko' on rapidapi.com
+                </li>
+                <li className="projects__content-covidTracker--description--details--content">
+                  Deployed address :{" "}
+                  <a href="https://yul761.github.io/covid_19_tracker/">
+                    https://yul761.github.io/covid_19_tracker/
+                  </a>
+                </li>
+                <li className="projects__content-covidTracker--description--details--content">
+                  Github address :{" "}
+                  <a href="https://github.com/yul761/covid_19_tracker">
+                    https://github.com/yul761/covid_19_tracker
+                  </a>
                 </li>
               </ul>
             </div>
